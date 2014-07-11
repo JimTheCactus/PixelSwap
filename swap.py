5378720463168aae2bc2c7ed97c884128580b8c2
JimTheCactus <jimthecactus@gmail.com>
2014-07-11 14:10:07 -0600

    Added commentary, fixed an issue where alpha images weren't handled right. (All images will be handled right now as a result, even grayscale and monochrome)

--- swap.py
+++ swap.py
@@ -1,42 +1,55 @@
 from PIL import Image
-import colorsys
 
 def GetLuminance(pixel):
+    # Extract the pixel channel data
     b, g, r = pixel
+    # and used the standard luminance curve to get luminance.
     return .3*r+.59*g+.11*b
 
 print "Putting pixels on the palette..."
-#Open up the image and get all of the pixels out of it. (Memory intensive!)
-palette = Image.open("6.png")
-pixelsP = []
-width,height = palette.size
-for y in range(height):
-    for x in range(width):
-        curpixel = palette.getpixel((x,y))
-        pixelsP.append((GetLuminance(curpixel),curpixel))
+# Open up the image and get all of the pixels out of it. (Memory intensive!)
+palette = Image.open("3.png").convert(mode="RGB")
+
+pixelsP = [] # Allocate the array
+width,height = palette.size # Unpack the image size
+for y in range(height): # Scan the lines
+    for x in range(width): # within the line, scan the pixels
+        curpixel = palette.getpixel((x,y)) # get the pixel
+        pixelsP.append((GetLuminance(curpixel),curpixel)) # and add a (luminance, color) tuple to the array.
 
 
-#sort the pixels
+# sort the pixels by the calculated luminescence
 pixelsP.sort()
 
-print "Getting the reference Picture"
-source = Image.open("2.png")
-pixelsR = []
-width,height = source.size
-for y in range(height):
-    for x in range(width):
-        curpixel = source.getpixel((x,y))
-        pixelsR.append((GetLuminance(curpixel),(x,y),curpixel))
+print "Getting the reference picture..."
+# Open up the image and get all of the pixels out of it. (Memory intensive!)
+source = Image.open("2.png").convert(mode="RGB")
+pixelsR = [] # Allocate the array
+width,height = source.size # Unpack the image size
+for y in range(height): # Scan the lines
+    for x in range(width): # within the line, scan the pixels
+        curpixel = source.getpixel((x,y)) # get the pixel
+        pixelsR.append((GetLuminance(curpixel),(x,y))) # and add a (luminance, position) tuple
 
-#Sort the Reference pixels
+# Sort the Reference pixels by luminance too
 pixelsR.sort()
 
-for p in range(len(pixelsR)):
-    l,cord,dummy = pixelsR[p]
-    pl,pixel = pixelsP[p]
-    source.putpixel(cord,pixel)
+# Now for the neat observation. Luminance matters more to humans than chromanance,
+# given this then we want to match luminance as well as we can. However, we have
+# a finite luminance distribution to work with. Since we can't change that, it's best
+# just to line the two images up, sorted by luminance, and just simply assign the
+# luminance directly. The chrominance will be all kinds of whack, but fixing that
+# by way of loose sorting possible chrominance errors takes this algorithm from O(n)
+# to O(n^2), which just takes forever (trust me, I've tried it.)
+
+print "Painting reference with palette..."
+for p in range(len(pixelsP)): # For each pixel in the palette
+    pl,pixel = pixelsP[p] # Grab the pixel from the palette
+    l,cord = pixelsR[p] # Grab the location from the reference
+    source.putpixel(cord,pixel) # and assign the pallet pixel to the refrence pixels place
 
 print "Applying fixative..."
+# save out the result.
 source.save("o.png","PNG")
 print "Done!"
 
