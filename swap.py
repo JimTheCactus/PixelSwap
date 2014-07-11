4ca16ec4a0d9749919e96a12d560cf45e86b52d5
JimTheCactus <jimthecactus@gmail.com>
2014-07-11 13:44:59 -0600

    Made the silliest algorithm. And yet it's pretty darn good for being as fast as it is.

--- swap.py
+++ swap.py
@@ -5,48 +5,36 @@ def GetLuminance(pixel):
     b, g, r = pixel
     return .3*r+.59*g+.11*b
 
-
 print "Putting pixels on the palette..."
 #Open up the image and get all of the pixels out of it. (Memory intensive!)
-palette = Image.open("1.png")
-pixels = []
-pixelsL = []
+palette = Image.open("6.png")
+pixelsP = []
 width,height = palette.size
 for y in range(height):
     for x in range(width):
         curpixel = palette.getpixel((x,y))
-        pixelsL.append((GetLuminance(curpixel),curpixel))
+        pixelsP.append((GetLuminance(curpixel),curpixel))
 
 
 #sort the pixels
-pixelsL.sort()
-pallen = len(pixelsL)
+pixelsP.sort()
 
-print "Painting new picture..."
+print "Getting the reference Picture"
 source = Image.open("2.png")
+pixelsR = []
 width,height = source.size
 for y in range(height):
-    print str(y) + " of " + str(height)
-    try:
-        source.save("o.png","PNG")
-    except:
-        pass
     for x in range(width):
-        l = GetLuminance(source.getpixel((x,y)))
-        bestdist = 256*256
-        best = 0
-        for pos in range(pallen):
-            pl,pixel = pixelsL[pos]
-            dist = (l-pl)
-            dist = dist*dist
-            if dist<bestdist:
-                    bestdist = dist
-                    bestpixel = pixel
-                    best = pos
-                    
-        source.putpixel((x,y),bestpixel)
-        pixelsL = pixelsL[0:best]+pixelsL[best+1:]
-        pallen = pallen-1
+        curpixel = source.getpixel((x,y))
+        pixelsR.append((GetLuminance(curpixel),(x,y),curpixel))
+
+#Sort the Reference pixels
+pixelsR.sort()
+
+for p in range(len(pixelsR)):
+    l,cord,dummy = pixelsR[p]
+    pl,pixel = pixelsP[p]
+    source.putpixel(cord,pixel)
 
 print "Applying fixative..."
 source.save("o.png","PNG")
